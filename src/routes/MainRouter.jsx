c5699b6182098b86b943fcc79c7281c363e12187
Ador Shahriar <ador.shahriar@gmail.com>
2023-10-17 20:03:08 +0600

    basic routers, layout added

---
+++ src/routes/MainRouter.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import MainLayout from '../layouts/MainLayout';
+import ErrorPage from '../pages/ErrorPage';
+import Home from '../pages/Home';
+
+const MainRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
+    },
+  ]);
+
+export default MainRouter;
