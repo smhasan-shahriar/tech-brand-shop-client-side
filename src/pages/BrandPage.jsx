b65dceb34b2e2112328ab3fdd9dd2f46a150f478
Ador Shahriar <ador.shahriar@gmail.com>
2023-10-19 01:17:36 +0600

    Brand Advertises updated

--- src/pages/BrandPage.jsx
+++ src/pages/BrandPage.jsx
@@ -4,51 +4,67 @@ import AwesomeSlider from "react-awesome-slider";
 import "react-awesome-slider/dist/styles.css";
 import Carousel from "nuka-carousel";
 import ProductCard from "../components/ProductCard";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const BrandPage = () => {
-    window.scrollTo({top: 0})
+  window.scrollTo({ top: 0 });
+  const [selectedBrand, setSelectedBrand] = useState([]);
+  useEffect(() => {
+    fetch("/data.json")
+      .then((res) => res.json())
+      .then((data) => setSelectedBrand(data));
+  }, []);
   const { name } = useParams();
+  const particularBrand = selectedBrand.find((item) => item.name === name);
+  const advertises = particularBrand?.advertise;
   const products = useLoaderData();
   const brand = name.toLowerCase();
   return (
-    <div className="max-w-[1400px] mx-auto mb-20">
-      {
-        products.length > 0 ? <div>
-<div>
-        <Carousel wrapAround={true} className="">
-          {products.slice(0,3).map((product) => (
-            <div key={product._id} className="flex">
-              <div
-                className="hero h-[400px]"
-                style={{
-                  backgroundImage:
-                    `url(${product.image})`,
-                }}
-              >
-                <div className="hero-overlay bg-opacity-60"></div>
-                <div className="hero-content text-center text-neutral-content p-20">
-                  <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Buy {product.name} at lowest price</h1>
-                    <button className="btn btn-primary normal-case">Use Coupon: {product.brand}10</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
-        </Carousel>
-      </div>
-      <h2 className="text-4xl font-bold my-10 text-center">Products from {name}</h2>
-      <div className="grid lg:grid-cols-2 gap-5 justify-items-center">
-          {
-            products.map(product => <ProductCard key={product._id} product={product}></ProductCard>)
-          }
-      </div>
+    <div className="max-w-[1400px] mx-auto mb-20 mt-10">
+      {products.length > 0 ? (
+        <div>
+          <div>
+            <Carousel wrapAround={true}>
+              {advertises?.map((advertise, index) => (
+                <img
+                  className="w-full h-[60vh] object-contain"
+                  key={index}
+                  src={advertise}
+                />
+              ))}
+            </Carousel>
+          </div>
 
-        </div> : <div className="h-[95vh] flex justify-center items-center text-6xl font-bold text-center">
-            Sorry, Products from this brand is currently unavailable
+          <h2 className="text-4xl font-bold my-10 text-center">
+            Products from {name}
+          </h2>
+          <div className="grid lg:grid-cols-2 gap-5 justify-items-center">
+            {products.map((product) => (
+              <ProductCard key={product._id} product={product}></ProductCard>
+            ))}
+          </div>
         </div>
-      }  
+      ) : (
+        <div>
+          <div>
+            <Carousel wrapAround={true}>
+              {advertises?.map((advertise, index) => (
+                <img
+                  className="w-full h-[60vh] object-contain"
+                  key={index}
+                  src={advertise}
+                />
+              ))}
+            </Carousel>
+          </div>
+          <div className="py-20 flex justify-center items-center text-6xl font-bold text-center">
+          Sorry, Products from this brand is currently unavailable
+        </div>
+        </div>
+
       
+      )}
     </div>
   );
 };
