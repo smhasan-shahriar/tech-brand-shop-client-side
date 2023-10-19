00e00fb8316b4f483469c657c26e14399a5b73d8
Ador Shahriar <ador.shahriar@gmail.com>
2023-10-19 23:18:11 +0600

    theme issue optimized

--- src/components/ThemeChanger.jsx
+++ src/components/ThemeChanger.jsx
@@ -9,8 +9,8 @@ const ThemeChanger = () => {
     };
   
     const buttonClass = isDark
-      ? 'bg-black text-white focus:outline-none m-1 rounded p-2 shadow-outline btn capitalize w-24 absolute md:static right-0 top-24'
-      : ' text-black m-1 rounded p-2 shadow-outline btn capitalize w-24 absolute md:static right-0 top-24';
+      ? 'bg-black text-white focus:outline-none m-1 rounded p-2 shadow-outline btn capitalize w-24 absolute md:static right-0 top-20'
+      : ' text-black m-1 rounded p-2 shadow-outline btn capitalize w-24 absolute md:static right-0 top-20';
   
     const buttonText = !isDark ? 'Turn on Dark Mode' : 'Turn Off Dark Mode';
   
